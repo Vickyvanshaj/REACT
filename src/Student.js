c237c7b5c1c782017119b35ef396a0e96b2735ba
vanshaj <vickyvanshajsingh@gmail.com>
2021-06-12 00:00:35 +0530

    States

--- src/Student.js
+++ src/Student.js
@@ -1,21 +1,43 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
-const Student = props =>{
-    return <React.Fragment>
-        <h1>Hello Everyone</h1>
-    <p>{props.children}</p>
-    </React.Fragment>
-}
-
-//class based
-// class Student extends Component
-// {
+// //state outside the constructer
+// class Student extends Component{
+//     state={
+//         name:"Vicky",
+//         age:24,
+//         branch:this.props.branch,
+//     }
+//     //props cant be modified but if you add props to state like above,you can modify it
 //     render()
 //     {
 //         return <React.Fragment>
-//         <h1>Hello Everyone</h1>
-//     <p>{this.props.children}</p>
-//     </React.Fragment>
+//             <h1>Hello {this.state.name}</h1>
+//             <p>Your age is {this.state.age}</p>
+//             <p>Your roll number is {this.props.roll}</p>
+//             <p>Your Branch is {this.state.branch}</p>
+//         </React.Fragment>
 //     }
 // }
+
+
+//state inside the constructor
+class Student extends Component{
+   constructor(props){
+       super(props);
+       this.state={
+        name:"Vicky",
+        age:24,
+        branch:this.props.branch,
+    };
+   }
+   render()
+    {
+        return <React.Fragment>
+            <h1>Hello {this.state.name}</h1>
+            <p>Your age is {this.state.age}</p>
+            <p>Your roll number is {this.props.roll}</p>
+            <p>Your Branch is {this.state.branch}</p>
+        </React.Fragment>
+    }
+}
 export default Student;
--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import ReactDOM from "react-dom";
 import React from "react";
 import Student from './Student';
 
-ReactDOM.render(<Student>I am child</Student>,document.getElementById('root')); 
+ReactDOM.render(<Student roll="177" branch="IT" />,document.getElementById('root')); 
