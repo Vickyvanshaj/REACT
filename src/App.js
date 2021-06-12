8e7ea2ec17eed32247fa9e10f0efa43703166240
vanshaj <vickyvanshajsingh@gmail.com>
2021-06-13 00:39:55 +0530

    mounting

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from "react";
-import Student from './Student';
-//creating component
-
-
-const App=()=>{
-    return <React.Fragment>
-      <Student name="vicky"/>
-      <Student name="rohan"/>
-      <Student name="yash" />
-    </React.Fragment>
+import React, { Component } from 'react'
+import Student from "./Student";
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    console.log('App inside constructor');
+    console.log(props);
+    this.state={
+      roll:"101",
+    };
+  }
+  static getDerivedStateFromProps(props,state)
+  {
+    console.log('getdeived');
+    console.log(props,state);
+    return null;
+  }
+  componentDidMount(){
+    //get data from server and set the states
+    console.log('component did mount');
+  }
+  render() {
+    console.log('render')
+    return (
+      <div>
+       <Student name="vivi"/>
+       <Student name="rohan"/>
+      </div>
+    )
+  }
 }
-export default App;
--- src/Student.js
+++ src/Student.js
@@ -1,27 +1,12 @@
 import React, { Component } from 'react'
 
-class Student extends Component{     
-   
-    state={
-        id:"12",
-        name:"Rahul",
-    };
-    handleClick = (id,name,e) =>{
-        console.log(id,name,e);
-    }
-   handleClickArg = (e)=>{
-       return this.handleClick(this.state.id)
-   }
-    render(){
-        return  <React.Fragment>
-            <h1>Hello {this.state.name}</h1>
-            <h1>Hello {this.props.roll}</h1>
-            
-            <button onClick={this.handleClick.bind(this,this.state.id,this.state.name)}>Delete</button>
-            <button onClick={(e)=>this.handleClick(this.state.id,this.state.name,e)}>Delete </button>
-            {/* <button onClick={this.handleClickArg}>Delete</button> */}
-
-        </React.Fragment>
+export default class Student extends Component {
+    render() {
+        console.log('student rendered');
+        return (
+            <div>
+                <h1>Hello {this.props.name}</h1>
+            </div>
+        )
     }
 }
-export default Student;
