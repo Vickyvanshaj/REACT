7314286294098dc6a0e285c7bce375d9bf29dcf6
vanshaj <vickyvanshajsingh@gmail.com>
2021-06-14 14:30:14 +0530

    custom Hook

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-import React, { useState,useEffect } from 'react'
+import React, { useState } from 'react'
+import useCustomCounter from './Custom';
+
 function  App() {
-    const [count,setCount]=useState(0);
-    const [count1,setCount1]=useState(10);
-    function handleIncrement() {
-        setCount(count+1);
-    }
-    const handleDecrement =()=>{
-        setCount1(count1-1);
-    }
-    //use effect is called when component is mounted,updated or unmounted
-    useEffect(() => {
-        
-       console.log('useEffect called')
-    },[count]);
-   //now useeffect will only run if count state is invloved
-    console.log('render')
+    const data=useCustomCounter();
     return (
-
         <React.Fragment>
-            <h1>Cout Up:{count} </h1>
-            <button type="button" onClick={handleIncrement}>Increment </button>
-
-            <h1>Cout Down:{count1} </h1>
-            <button type="button" onClick={handleDecrement}>Decrement </button>
+            <h1>Count UP: {data.count}</h1>
+            <button type="button" onClick={data.handleIncrement}></button>
         </React.Fragment>
     )
 }
