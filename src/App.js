17a86571455aceacf9ae7326c0b3a2a8ba5557ab
vanshaj <vickyvanshajsingh@gmail.com>
2021-06-14 15:51:25 +0530

    login logout using IIFE

--- src/App.js
+++ src/App.js
@@ -3,9 +3,26 @@ import User from './User';
 import Guest from './Guest';
 const App=()=>{
     const [isLoggedIn,setLog]=useState(true);
-    return <React.Fragment>
-    {(isLoggedIn==true)?<User setLogfn={setLog}/>:<Guest setLogfn={setLog}/>}
-    </React.Fragment>
+    // return <React.Fragment>
+    // {(isLoggedIn==true)?<User setLogfn={setLog}/>:<Guest setLogfn={setLog}/>}
+    // </React.Fragment>
    
+    return (
+        <div>
+            {
+                (
+                    ()=>{
+                        if(isLoggedIn)
+                        {
+                            return <User setLogfn={setLog}/>;
+                        }
+                        else{
+                            return <Guest setLogfn={setLog}/>;
+                        }
+                    })()
+                
+            }
+        </div>
+    )
 }
 export default App;
