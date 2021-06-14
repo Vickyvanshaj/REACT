86cbbfd73cf491a69d7639534a55b39dd5096718
vanshaj <vickyvanshajsingh@gmail.com>
2021-06-14 15:35:29 +0530

    login logout using if

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React,{useState} from 'react';
 import User from './User';
-const App=(props)=>{
-    console.log(props)
-    const prime=props.primeMember;
-    return <React.Fragment>
-        <h1>Welcome User</h1>
-        {prime && <User/>}
-    </React.Fragment>
+import Guest from './Guest';
+const App=()=>{
+    const [isLoggedIn,setLog]=useState(true);
+    if(isLoggedIn)
+    return <User setLogfn={setLog}/>
+    else
+    return <Guest setLogfn={setLog}/>
+   
 }
 export default App;
--- src/User.js
+++ src/User.js
@@ -1,10 +1,12 @@
-import React,{Fragment} from 'react'
-import ReactDOM from 'react-dom'
-
-
-function User(){
+import React from 'react'
+const User=(props)=>{
+    const logout=()=>{
+        const setLog=props.setLogfn;
+        setLog(false);
+    }
     return <React.Fragment>
-        <h1>Prime Membership</h1>
+        <h1>Welcome Vanshaj</h1>
+        <button onClick={logout}>Logout</button>
     </React.Fragment>
 }
 export default User;
