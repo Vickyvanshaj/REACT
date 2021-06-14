bfefbc5c22c9a5c2f479c112d95df004decd8681
vanshaj <vickyvanshajsingh@gmail.com>
2021-06-14 13:48:05 +0530

    useEffect Hook

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import React, { useState } from 'react'
-
-function App(){
-    const [name,setName]=useState("Rahul");
-    const [roll,setRoll]=useState(101);
-    //you cant use setName here..it will go in the infinte loop. rather use inside an event handler
-
-    const handleClick = () =>{
-        setName("Vanshaj");
-        setRoll('188');
+import React, { useState,useEffect } from 'react'
+function  App() {
+    const [count,setCount]=useState(0);
+    const [count1,setCount1]=useState(10);
+    function handleIncrement() {
+        setCount(count+1);
     }
-    return <React.Fragment>
-        <h1> Hello {name}</h1>
-        <p>Your roll is {roll}</p>
-        <button onClick={handleClick}>Change</button>
-    </React.Fragment>
+    const handleDecrement =()=>{
+        setCount1(count1-1);
+    }
+    //use effect is called when component is mounted,updated or unmounted
+    useEffect(() => {
+        
+       console.log('useEffect called')
+    },[count]);
+   //now useeffect will only run if count state is invloved
+    console.log('render')
+    return (
+
+        <React.Fragment>
+            <h1>Cout Up:{count} </h1>
+            <button type="button" onClick={handleIncrement}>Increment </button>
+
+            <h1>Cout Down:{count1} </h1>
+            <button type="button" onClick={handleDecrement}>Decrement </button>
+        </React.Fragment>
+    )
 }
 export default App;
