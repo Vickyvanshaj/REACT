c908a57be6e804c94a45a7f44539d5ca3dc41bdb
vanshaj <vickyvanshajsingh@gmail.com>
2021-06-16 18:06:43 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react'
-import Rohan from './Rohan';
-import Misba from './Misba';
-const App=()=>{
+import Nav from './Nav'
+import About from './About'
+import Shop from './Shop'
+import "./App.css";
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
+
+function App() {
     return (
+        <Router>
         <div>
-            <Rohan/>
-            <Misba/>
+            <Nav/>  
+            <Switch>
+            
+            <Route path="/" exact component={Home}/>
+            <Route path="/about" component={About}/>
+            <Route path="/shop" component={Shop}/>
+            </Switch>
         </div>
+        </Router>
     )
 }
-export default App;
+
+const Home=()=>{
+    return <h1>Home Page</h1>
+}
+export default App
